d6095ba514aa760fadc4448fcb2f8a0b39fc533a
Paul Shorey <ps@artspaces.net>
2020-12-06 14:31:47 -0500

    debug - break cache - change variable name temporarily

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import cconsoleInit from "./src/index.js"
 
 
 if (typeof window === 'object') {
-  window.cconsole = cconsoleInit()
+  window.myconsole = cconsoleInit()
 }
 
 export default cconsoleInit
